c9d20bd37a250a17cec1baf5a5ff6ad71982829f
rain <shuyeyaojing@foxmail.com>
2024-06-26 13:56:00 +0800

    feat(redis): add channel and msgChannel to RedisSubscribe
    Added new fields to the RedisSubscribe struct to handle subscriptionchannels and message channels more efficiently. Modified the NewRedisSubscribe
    constructor to initialize these fields and introduced a context cancellationfunction for proper resource management. Refactored PubMessage and SubMessage
    methods to use the new channel field and added a Close method to cancel
    subscriptions gracefully.

--- common/go_redis/subscribe.go
+++ common/go_redis/subscribe.go
@@ -2,30 +2,45 @@ package go_redis
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-redis/redis/v8"
 )
 
 type RedisSubscribe struct {
-	conn *redis.Client
+	ctx        context.Context
+	conn       *redis.Client
+	channel    string
+	msgChannel chan string
+	cancel     func()
 }
 
-func NewRedisSubscribe(conn *redis.Client) *RedisSubscribe {
-	return &RedisSubscribe{conn: conn}
+func NewRedisSubscribe(conn *redis.Client, channel string, msgChannel chan string) *RedisSubscribe {
+	ctx, cancel := context.WithCancel(context.TODO())
+	return &RedisSubscribe{ctx: ctx, cancel: cancel, conn: conn, channel: channel, msgChannel: msgChannel}
 }
 
-func (r *RedisSubscribe) PubMessage(ctx context.Context, channel, msg string) {
-	r.conn.Publish(ctx, channel, msg)
+func (r *RedisSubscribe) PubMessage(ctx context.Context, msg string) {
+	r.conn.Publish(ctx, r.channel, msg)
 }
 
-func (r *RedisSubscribe) SubMessage(ctx context.Context, channel, msg string) {
-	pubsub := r.conn.Subscribe(ctx, channel)
+func (r *RedisSubscribe) SubMessage(ctx context.Context) {
+	pubsub := r.conn.Subscribe(ctx, r.channel)
 	_, err := pubsub.Receive(ctx)
 	if err != nil {
 		panic(err)
 	}
 	ch := pubsub.Channel()
-	for msg := range ch {
-		fmt.Println(msg.Channel, msg.Payload)
-	}
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			case msg := <-ch:
+				r.msgChannel <- msg.Payload
+			}
+		}
+	}()
+}
+
+func (r *RedisSubscribe) Close() {
+	r.cancel()
 }
