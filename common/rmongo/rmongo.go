916a9120042e4a83f8d357f54c270b26a5d38617
rain <shuyeyaojing@foxmail.com>
2025-07-25 20:33:03 +0800

    refactor(rredis/rmongo):优化 Redis 和 Mongo 连接池
    - 为 Redis 和 Mongo客户端添加连接池，避免重复创建连接
    - 优化 Mongo 客户端初始化逻辑，移除单例模式
    - 为 Redis 客户端添加错误处理和配置验证

--- common/rmongo/rmongo.go
+++ common/rmongo/rmongo.go
@@ -12,30 +12,30 @@ import (
 
 var ProviderSet = wire.NewSet(NewMongo)
 
-var mc *mongo.Client
-var mcOnce = sync.Once{}
+var mcMap = sync.Map{}
 
 func NewMongo(ctx context.Context, mogConf *MongoCfg, logger log.Logger) (*mongo.Database, error) {
-	if mc != nil {
+	var mci, _ = mcMap.Load(mogConf.Url)
+	if mci != nil {
+		mc := mci.(*mongo.Client)
 		return mc.Database(mogConf.Db), nil
 	}
-	var err error
-	mcOnce.Do(func() {
-		clientOption := options.Client().ApplyURI(mogConf.Url).SetConnectTimeout(time.Duration(mogConf.ConnectTimeoutMS) * time.Millisecond)
-		if mogConf.MaxPoolSize > 0 {
-			clientOption = clientOption.SetMaxPoolSize(mogConf.MaxPoolSize)
-		}
-		if mogConf.MaxPoolSize > 0 {
-			clientOption = clientOption.SetMinPoolSize(mogConf.MaxPoolSize)
-		}
-		if mogConf.MaxIdleTimeMS > 0 {
-			clientOption = clientOption.SetMaxConnIdleTime(time.Duration(mogConf.MaxIdleTimeMS) * time.Millisecond)
-		}
-		mc, err = mongo.Connect(ctx, clientOption)
-	})
+	clientOption := options.Client().ApplyURI(mogConf.Url).SetConnectTimeout(time.Duration(mogConf.ConnectTimeoutMS) * time.Millisecond)
+	if mogConf.MaxPoolSize > 0 {
+		clientOption = clientOption.SetMaxPoolSize(mogConf.MaxPoolSize)
+	}
+	if mogConf.MaxPoolSize > 0 {
+		clientOption = clientOption.SetMinPoolSize(mogConf.MaxPoolSize)
+	}
+	if mogConf.MaxIdleTimeMS > 0 {
+		clientOption = clientOption.SetMaxConnIdleTime(time.Duration(mogConf.MaxIdleTimeMS) * time.Millisecond)
+	}
+	mc, err := mongo.Connect(ctx, clientOption)
 	if err != nil {
 		log.NewHelper(logger).Errorf("mongo open error: %s, mogConf %#v \n", err.Error(), mogConf)
 		return nil, err
 	}
+	mcMap.Store(mogConf.Url, mc)
+	log.NewHelper(logger).Infof("mongo open success: mogConf %#v \n", mogConf)
 	return mc.Database(mogConf.Db), nil
 }
--- common/rredis/redis.go
+++ common/rredis/redis.go
@@ -2,18 +2,34 @@ package rredis
 
 import (
 	"context"
+	"errors"
 	"github.com/go-redis/redis/v8"
 	"github.com/google/wire"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
 var ProviderSet = wire.NewSet(NewRedis)
+
+var clientMap = sync.Map{}
 
 type Client struct {
 	redis.Cmdable
 }
 
 func NewRedis(o *RedisCfg, ctx context.Context) (client *Client, err error) {
+	if o == nil || len(o.Addr) == 0 {
+		return nil, errors.New("redis config error")
+	}
+
+	key := strings.Join(o.Addr, "_") + "_" + strconv.FormatInt(o.DB, 10)
+	c, _ := clientMap.Load(key)
+	if c != nil {
+		client = c.(*Client)
+		return
+	}
 	var redisCli redis.Cmdable
 	if len(o.Addr) > 1 {
 		redisCli = redis.NewClusterClient(
@@ -46,6 +62,7 @@ func NewRedis(o *RedisCfg, ctx context.Context) (client *Client, err error) {
 
 	client = new(Client)
 	client.Cmdable = redisCli
+	clientMap.Store(key, client)
 	return client, nil
 }
 
